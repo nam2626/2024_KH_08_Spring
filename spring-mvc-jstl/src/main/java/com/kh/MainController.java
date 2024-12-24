9692aed0d6450554043329846ed8945d6a128ae5
nam2626 <nam2626@naver.com>
2024-12-24 16:01:11 +0900

    request 객체 없이 바로 파라미터 받는 방법

--- spring-mvc-jstl/src/main/java/com/kh/MainController.java
+++ spring-mvc-jstl/src/main/java/com/kh/MainController.java
@@ -3,6 +3,7 @@ package com.kh;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,19 +22,26 @@ public class MainController {
 	public String registerView() {
 		return "member_register";
 	}
+
+	
+	/*
+	 * @PostMapping("/login.do") public String login(HttpServletRequest request,
+	 * HttpSession session) { //login.jsp에서 보낸 아이디와 암호를 받아서 sysout으로 출력 String id =
+	 * request.getParameter("id"); String pass = request.getParameter("pass");
+	 * System.out.println(id + " " + pass); //아이디와 암호를 세션에 저장해서 login_result.jsp로 이동
+	 * //login_result.jsp에서는 세션에 저장한 아이디 암호 값을 출력 // HttpSession session =
+	 * request.getSession(); session.setAttribute("id", id);
+	 * session.setAttribute("pass", pass);
+	 * 
+	 * return "login_result"; }
+	 */
 	
 	@PostMapping("/login.do")
-	public String login(HttpServletRequest request, HttpSession session) {
-		//login.jsp에서 보낸 아이디와 암호를 받아서 sysout으로 출력
-		String id = request.getParameter("id");
-		String pass = request.getParameter("pass");
+	public String login(@RequestParam(name = "id", defaultValue = "user", required = true)String id, 
+			@RequestParam(name = "pass", defaultValue = "password", required = true) String pass, HttpSession session) {
 		System.out.println(id + " " + pass);
-		//아이디와 암호를 세션에 저장해서 login_result.jsp로 이동
-		//login_result.jsp에서는 세션에 저장한 아이디 암호 값을 출력
-//		HttpSession session = request.getSession();
 		session.setAttribute("id", id);
 		session.setAttribute("pass", pass);
-		
 		return "login_result";
 	}
 	
