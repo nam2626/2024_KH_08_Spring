b25063462462fd3bc245c53c61ebb43f4fc178ed
nam2626 <nam2626@naver.com>
2025-01-06 15:58:46 +0900

    게시글 목록 읽어오는 처리

--- spring-board/src/main/java/com/kh/controller/HomeController.java
+++ spring-board/src/main/java/com/kh/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.kh.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.dto.BoardDTO;
 import com.kh.service.BoardService;
+import com.kh.vo.PaggingVO;
 
 @Controller
 public class HomeController {
@@ -19,13 +23,14 @@ public class HomeController {
   public ModelAndView index(@RequestParam(defaultValue = "1") int pageNo,
     @RequestParam(defaultValue = "30") int pageContentEa, ModelAndView view) {
       //전체 게시글 개수 조회
-
+      int count = boardService.selectBoardTotalCount();
       //페이지 번호를 보내서 해당 페이지 게시글 목록만 조회
-
+      List<BoardDTO> list = boardService.getBoardList(pageNo, pageContentEa);
       //PaggingVO 페이징 정보 생성
-
+      PaggingVO pagging = new PaggingVO(count, pageNo, pageContentEa);
       //데이터 추가
-
+      view.addObject("list", list);
+      view.addObject("pagging", pagging);
       //이동할 페이지 설정
       view.setViewName("index");
 
