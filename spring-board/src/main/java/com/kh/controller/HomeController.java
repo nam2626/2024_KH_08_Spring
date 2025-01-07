c10bd6cb2ce80ec084e7755ad9876f8558078b36
nam2626 <nam2626@naver.com>
2025-01-07 14:47:00 +0900

    로그인 처리

--- spring-board/src/main/java/com/kh/controller/HomeController.java
+++ spring-board/src/main/java/com/kh/controller/HomeController.java
@@ -1,22 +1,31 @@
 package com.kh.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.dto.BoardDTO;
+import com.kh.dto.BoardMemberDTO;
+import com.kh.service.BoardMemberService;
 import com.kh.service.BoardService;
 import com.kh.vo.PaggingVO;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
   private BoardService boardService;
+  private BoardMemberService boardMemberService;
 
-  public HomeController(BoardService boardService) {
+  public HomeController(BoardService boardService, BoardMemberService boardMemberService) {
     this.boardService = boardService;
+    this.boardMemberService = boardMemberService;
   }
 
   @GetMapping({"/", "/main"})
@@ -42,5 +51,22 @@ public class HomeController {
   public String loginView() {
       return "login";
   }
+
+  @PostMapping("/login")
+  public String login(String id, String password, HttpSession session, HttpServletResponse response) throws IOException {
+      BoardMemberDTO member = boardMemberService.login(id, password);
+      if(member == null) {
+        session.setAttribute("user", member);
+        return "redirect:/main";
+      }
+      response.setContentType("text/html; charset=utf-8");
+      response.getWriter().println(
+        "<script>alert('로그인 실패\\n아이디와 비밀번호 확인하세요');" + 
+        "history.back();</script>");
+
+
+      return null;
+  }
+  
   
 }
