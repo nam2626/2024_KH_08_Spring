019b19d7aeff4645c089b314e744e5b1a5929b51
nam2626 <nam2626@naver.com>
2025-01-07 17:20:25 +0900

    회원정보 삭제

--- spring-board/src/main/java/com/kh/controller/MemberController.java
+++ spring-board/src/main/java/com/kh/controller/MemberController.java
@@ -1,10 +1,16 @@
 package com.kh.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.dto.BoardMemberDTO;
@@ -27,6 +33,25 @@ public class MemberController {
       view.addObject("list", list);
       view.setViewName("admin_member_main");
       return view;
+  }
+
+  @ResponseBody
+  @DeleteMapping("/delete")
+  public Map<String,Object> deleteMember(@RequestBody Map<String, Object> param) {
+    HashMap<String, Object> map = new HashMap<String, Object>();
+    String id = param.get("id").toString();
+    int count = service.deleteMember(id);
+    
+    if(count > 0) {
+      map.put("msg", id + " - 해당 아이디 삭제 완료");
+    }else{
+      map.put("msg", id + " - 삭제할 아이디를 찾을 수 없습니다.");
+    }
+
+    List<BoardMemberDTO> list = service.selectAllMember();
+    map.put("list", list);
+
+    return map;
   }
   
 }
