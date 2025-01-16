b3486f84009382f5f114eaad612921c04df9654d
nam2626 <nam2626@naver.com>
2025-01-16 13:47:44 +0900

    게시글 조회 컨트롤러 작성

--- spring-board-api/src/main/java/com/kh/controller/BoardController.java
+++ spring-board-api/src/main/java/com/kh/controller/BoardController.java
@@ -1,19 +1,25 @@
 package com.kh.controller;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.dto.BoardCommentDTO;
 import com.kh.dto.BoardDTO;
+import com.kh.dto.BoardFileDTO;
 import com.kh.service.BoardService;
 import com.kh.token.JwtTokenProvider;
 import com.kh.vo.PaggingVO;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -21,7 +27,7 @@ public class BoardController {
 
 	private BoardService boardService;
 	private JwtTokenProvider tokenProvider;
-	
+
 	public BoardController(BoardService boardService, JwtTokenProvider tokenProvider) {
 		this.boardService = boardService;
 		this.tokenProvider = tokenProvider;
@@ -30,6 +36,7 @@ public class BoardController {
 	@GetMapping("/board/list")
 	public Map<String, Object> index(@RequestParam(defaultValue = "1") int pageNo,
 			@RequestParam(defaultValue = "30") int pageContentEa) {
+		System.out.println(pageNo);
 		// 전체 게시글 개수 조회
 		int count = boardService.selectBoardTotalCount();
 		// 페이지 번호를 보내서 해당 페이지 게시글 목록만 조회
@@ -40,7 +47,21 @@ public class BoardController {
 		Map<String, Object> map = new HashMap<>();
 		map.put("boardList", list);
 		map.put("pagging", pagging);
+
+		return map;
+	}
+
+	@GetMapping("/board/{bno}")
+	public Map<String, Object> boardDetail(@PathVariable int bno) {
+		Map<String, Object> map = new HashMap<>();
+		BoardDTO board = boardService.selectBoard(bno);
+		List<BoardCommentDTO> commentList = boardService.getCommentList(bno, 1);
+		List<BoardFileDTO> fileList = boardService.getBoardFileList(bno);
 		
+		map.put("board", board);
+		map.put("commentList", commentList);
+		map.put("fileList", fileList);
+
 		return map;
 	}
 
