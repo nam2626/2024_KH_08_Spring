05b1b046431ca46c76aef5441c0fd1bc9aec2e0c
nam2626 <nam2626@naver.com>
2025-01-17 09:53:27 +0900

    게시글 좋아요 처리

--- spring-board-api/src/main/java/com/kh/controller/BoardController.java
+++ spring-board-api/src/main/java/com/kh/controller/BoardController.java
@@ -8,17 +8,21 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.dto.BoardCommentDTO;
 import com.kh.dto.BoardDTO;
 import com.kh.dto.BoardFileDTO;
+import com.kh.dto.BoardMemberDTO;
 import com.kh.service.BoardService;
 import com.kh.token.JwtTokenProvider;
 import com.kh.vo.PaggingVO;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
@@ -57,11 +61,41 @@ public class BoardController {
 		BoardDTO board = boardService.selectBoard(bno);
 		List<BoardCommentDTO> commentList = boardService.getCommentList(bno, 1);
 		List<BoardFileDTO> fileList = boardService.getBoardFileList(bno);
-		
+
 		map.put("board", board);
 		map.put("commentList", commentList);
 		map.put("fileList", fileList);
 
+		return map;
+	}
+
+	@GetMapping("/board/comment/{bno}")
+	public List<BoardCommentDTO> getMethodName(@PathVariable int bno, @RequestParam int start) {
+		List<BoardCommentDTO> commentList = boardService.getCommentList(bno, start);
+		return commentList;
+	}
+
+	@GetMapping("/like/{bno}")
+	public Map<String, Object> boardLike(@PathVariable int bno,
+			@RequestHeader("Authorization") String token) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		token = token != null ? token.replace("Bearer ", "") : null;
+		if (token == null) {
+			map.put("code", 2);
+			map.put("msg", "로그인 하셔야 이용하실수 있습니다.");
+		} else {
+			String id = tokenProvider.getUserIDFromToken(token);
+			try {
+				boardService.insertBoardLike(bno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 게시글에 좋아요 하셨습니다.");
+			} catch (Exception e) {
+				boardService.deleteBoardLike(bno, id);
+				map.put("code", 1);
+				map.put("msg", "해당 게시글에 좋아요를 취소 하셨습니다.");
+			}
+			map.put("count", boardService.getBoardLike(bno));
+		}
 		return map;
 	}
 
