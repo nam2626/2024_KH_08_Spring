6fa6fbca3c3bd88269044d9ac6f9548d62446178
nam2626 <nam2626@naver.com>
2025-02-05 12:13:33 +0900

    테스트 메서드 추가

--- SpringAPIMember/src/test/java/com/member/SpringApiMemberApplicationTests.java
+++ SpringAPIMember/src/test/java/com/member/SpringApiMemberApplicationTests.java
@@ -1,7 +1,12 @@
 package com.member;
 
 import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -13,6 +18,8 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -81,6 +88,52 @@ class SpringApiMemberApplicationTests {
 	public void deleteTest() {
 		int result = service.deleteMember("test02");
 		assertEquals(1, result, "회원 정보 삭제 실패");
+	}
+	
+	//회원 아이디 검색
+	//	데이터 3개를 검색한 결과
+	//	테스트할 데이터 "fo1311","px2959","sm9203"
+	//	assertNotNull(결과값, 실패했을때 메세지)
+	@DisplayName("검색 테스트")
+	@Order(3)
+	@ParameterizedTest
+	@ValueSource(strings = {"user021","user019","user022","admin"})
+	public void selectTest(String value) {
+		assertNotNull(service.selectMember(value),value + " - 검색 테스트 실패");
+	}
+	
+	@DisplayName("배열 비교 테스트")
+	@Order(4)
+	@Test
+	public void arrayTest() {
+		assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 2, 3});
+	}
+	
+	@DisplayName("여러개의 테스트 확인")
+	@Order(5)
+	@Test
+	public void assertAllTest() {
+		assertAll(
+		    () -> assertEquals(4, 2 + 2),
+		    () -> assertTrue(3 < 2)
+		);
+	}
+	
+	@DisplayName("결과값 True/False 테스트 확인")
+	@Order(6)
+	@Test
+	public void assertTrueFalseTest() {
+		assertTrue(3 > 2);
+//		assertFalse(3 < 2);
+
+	}
+	@DisplayName("결과값 Null/Not Null 테스트 확인")
+	@Order(7)
+	@Test
+	public void assertNullNotNullTest() {
+		assertNull(null);
+//		assertNotNull(new Object());
+
 	}
 }
 
